808da3f5bb82acf8e1bcf45714fa1b4e10461017
C Fu <C Fu>
2017-01-08 01:13:18 -0800

    description

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,113 +1,60 @@
 /*
 Here is where you create all the functions that will do the routing for your app, and the logic of each route.
 */
-
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
-router.get('/', function (req, res) {
-  return models.burger.findAll()
-  .then(function(burgers) {
-    return res.render('index', {burgers: burgers});
-  })
+router.get('/', function(req, res) {
+    return models.burger.findAll()
+        .then(function(burgers) {
+            return res.render('index', {
+                burgers: burgers
+            });
+        })
 });
 
 
-router.post('/create', function (req, res) {
-  // SOLUTION:
-  // =========
-  // use the Cat model to create a cat based on what's
-  // passed in req.body (name, sleepy, user_id)
-  models.burger.create({
-    burger_name: req.body.name,
-    devoured: req.body.devoured,
-  })
-  // connect the .create to this .then
-  .then(function() {
-    res.redirect('/');
-  });
+router.post('/create', function(req, res) {
+
+    models.burger.create({
+            burger_name: req.body.name,
+            devoured: req.body.devoured,
+        })
+        // connect the .create to this .then
+        .then(function() {
+            res.redirect('/');
+        });
 });
 
-router.put('/update/:id', function (req, res) {
-  models.burger.update({
-      devoured: req.body.devoured
-    },{
-    where: { id : req.params.id }
-    })
-  .then(function (result) {
-      res.redirect('/');
-    }, function(rejectedPromiseError){
+router.put('/update/:id', function(req, res) {
 
-    });
+    models.burger.update({
+            devoured: req.body.devoured
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(function(result) {
+            res.redirect('/');
+        }, function(rejectedPromiseError) {
+
+        });
 });
 
 
-router.delete('/delete/:id', function (req, res) {
-   models.burger.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-  // connect it to this .then.
-  .then(function() {
-    res.redirect('/');
-  });
+router.delete('/delete/:id', function(req, res) {
+    models.burger.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        // connect it to this .then.
+        .then(function() {
+            res.redirect('/');
+        });
 
 });
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//FROM ORM===================================================//
-// var express = require("express");
-// var router = express.Router();
-// var burger = require("../models/burger.js");
-
-// router.get('/', function(req, res)  {
-//   res.redirect('/burgers');
-// });
-
-// router.get('/burgers', function(req, res) {
-
-//   burger.selectAll(function(data) {
-//     res.render('index',{burgers: data})
-//   });
-
-// });
-
-// router.post('/burgers/create', function(req, res) {
-
-//   burger.insertOne(req.body.burgerName, req.body.devoured, function() {
-//     res.redirect('/burgers');
-//   });
-
-// });
-
-// router.put('/burgers/update/devour/:id', function(req, res)  {
-
-//   burger.updateOne(req.params.id, function() {
-//     res.redirect('/burgers');
-//   });
-
-// });
-
-// module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -40,31 +40,3 @@ module.exports = function(sequelize, DataTypes) {
 
 
 
-
-
-
-//FROM ORM=================================================================//
-// var orm = require("../config/orm.js");
-
-// var model = {
-
-//   selectAll : function(callback)  {
-//     orm.selectAll("burgers", function(data) {
-//       callback(data);
-//     });
-//   },
-
-//   insertOne : function(burgerName, burgerDevoured, callback)  {
-//     orm.insertOne("burgers", burgerName, burgerDevoured, function(data) {
-//       callback(data);
-//     });
-//   },
-
-//   updateOne : function(burgerID, callback)  {
-//     orm.updateOne("burgers", burgerID, function(data) {
-//       callback(data);
-//     });
-//   }
-// };
-
-// module.exports = model;
